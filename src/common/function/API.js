afa1c14360f294e1d1cb441ca02149bb125eaa21
Paulina Johnsson <paulina_johnsson@hotmail.com>
2018-05-30 20:19:37 +0200

    Lagt till celciuse.

--- src/common/function/API.js
+++ src/common/function/API.js
@@ -13,7 +13,7 @@ export const getBusiness = () => {
 
   export const getWeather = () => {
     try {
-      return axios.get('http://api.openweathermap.org/data/2.5/weather?q=Trollh%C3%A4ttan&APPID=430e77b06ba15e65d8e31f41dbfbb1fc');
+      return axios.get('http://api.openweathermap.org/data/2.5/weather?q=Trollh%C3%A4ttan&units=metric&APPID=430e77b06ba15e65d8e31f41dbfbb1fc');
     } catch (error) {
       console.error(error);
     }
